531c976eb60173099a3d3932b52ccf4340ede505
nirmal0001 <kdjj18322@gmail.com>
2025-06-08 09:42:32 +0530

    finsh home page css and html

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import "./styles.css"
+
+const navButtons = document.querySelectorAll("nav button");
+
+navButtons.forEach()
+
+class cafe {
+    constructor(){
+        this.navButtons = null;
+        this.main = null;
+    }
+    init(){
+
+    }
+
+    dom(){
+        this.navButtons = document.querySelectorAll("nav button");
+        this.main = document.querySelector('#content')
+    }
+}
